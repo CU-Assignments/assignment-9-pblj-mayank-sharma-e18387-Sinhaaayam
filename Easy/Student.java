9382355cd0c49fec799a2adae4ea900e5310c653
Sinhaaayam <135627202+Sinhaaayam@users.noreply.github.com>
2025-04-23 12:15:05 +0530

    Create Student.java

---
+++ Easy/Student.java
@@ -0,0 +1,17 @@
+package com.example.springdemo;
+
+public class Student {
+    private String name;
+    private Course course;
+
+    public Student(String name, Course course) {
+        this.name = name;
+        this.course = course;
+    }
+
+    public void printDetails() {
+        System.out.println("Student Name: " + name);
+        System.out.println("Enrolled Course: " + course.getCourseName());
+        System.out.println("Course Duration: " + course.getDuration() + " weeks");
+    }
+}
